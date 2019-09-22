b7ee87aeb6d339c9597425b3aaa5576952a2f8de
Dinho <dnavas@sdd.com.ar>
2019-09-22 12:13:46 -0300

    first commit wn.movie-info-service

---
+++ src/main/java/com/wagdynavas/wnmovieinfoservice/WnMovieInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wagdynavas.wnmovieinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnMovieInfoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WnMovieInfoServiceApplication.class, args);
+    }
+
+}
